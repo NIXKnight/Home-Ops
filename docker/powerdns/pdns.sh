3e22bdfd2d0a00189c191aad5743d65a8cfb9282
Saad Ali <engr.saadali786@gmail.com>
2024-04-20 13:55:29 +0500

    Reworked PowerDNS config generation

--- docker/powerdns/pdns.sh
+++ docker/powerdns/pdns.sh
@@ -19,37 +19,68 @@ function check_vars {
 function generate_pdns_config {
   : ${PDNS_API_ALLOW_FROM:=127.0.0.1}
   export PDNS_API_ALLOW_FROM
-  check_vars PDNS_MYSQL_HOST PDNS_MYSQL_USERNAME PDNS_MYSQL_PASSWORD PDNS_MYSQL_DATABASE PDNS_LOCAL_PORT PDNS_API_KEY PDNS_API_ADDRESS PDNS_API_PORT PDNS_API_ALLOW_FROM
+  check_vars PDNS_DB_BACKEND PDNS_DB_HOST PDNS_DB_USERNAME PDNS_DB_PASSWORD PDNS_DB_DATABASE PDNS_LOCAL_PORT PDNS_API_KEY PDNS_API_ADDRESS PDNS_API_PORT PDNS_API_ALLOW_FROM
   envsubst < /etc/powerdns/template/pdns.conf.template > /etc/powerdns/pdns.conf
 }
 
 # Function to generate PowerDNS Recursor configuration
 function generate_recursor_config {
-  check_vars PDNS_RECURSOR_LOCAL_ZONE PDNS_RECURSOR_LOCAL_DNS_ADDRESS PDNS_RECURSOR_LOCAL_DNS_PORT PDNS_RECURSOR_UPSTREAM_RESOLVERS
+  CPU_CORES=$(nproc)
+  HALF_CPU_CORES=$((CPU_CORES / 2))
+  if [ "$HALF_CPU_CORES" -lt 2 ]; then
+    export PDNS_RECURSOR_THREADS=2
+  else
+    export PDNS_RECURSOR_THREADS=$HALF_CPU_CORES
+  fi
+  echo -e "PDNS Recursor threads set to $PDNS_RECURSOR_THREADS"
+  check_vars PDNS_RECURSOR_LOCAL_ZONE PDNS_RECURSOR_LOCAL_DNS_ADDRESS PDNS_RECURSOR_LOCAL_DNS_PORT PDNS_RECURSOR_UPSTREAM_RESOLVERS PDNS_RECURSOR_MAX_CACHE_ENTRIES PDNS_RECURSOR_RECEIVE_BUFFER_SIZE PDNS_RECURSOR_SEND_BUFFER_SIZE
   envsubst < /etc/powerdns/template/recursor.conf.template > /etc/powerdns/recursor.conf
+}
+
+# Function to check and initialize PowerDNS Server
+function check_init_powerdns_server {
+  case "$PDNS_DB_BACKEND" in
+    gmysql)
+      DB_BACKEND_NAME="MySQL"
+      DB_PORT=3306
+      DB_CHECK_CMD="mysql -h $PDNS_DB_HOST -u $PDNS_DB_USERNAME -p$PDNS_DB_PASSWORD -D $PDNS_DB_DATABASE -e \"SHOW TABLES LIKE 'domains';\" 2>/dev/null | grep -q 'domains'"
+      DB_INIT_CMD="mysql -h $PDNS_DB_HOST -u $PDNS_DB_USERNAME -p$PDNS_DB_PASSWORD -D $PDNS_DB_DATABASE < /usr/share/pdns-backend-mysql/schema/schema.mysql.sql"
+      ;;
+    gpgsql)
+      DB_BACKEND_NAME="PostgreSQL"
+      DB_PORT=5432
+      export PGPASSWORD=$PDNS_DB_PASSWORD
+      DB_CHECK_CMD="psql -h $PDNS_DB_HOST -U $PDNS_DB_USERNAME -d $PDNS_DB_DATABASE -tAc \"SELECT 1 FROM pg_tables WHERE tablename = 'domains'\" 2>/dev/null | grep -q '1'"
+      DB_INIT_CMD="psql -h $PDNS_DB_HOST -U $PDNS_DB_USERNAME -d $PDNS_DB_DATABASE -f /usr/share/pdns-backend-pgsql/schema/schema.pgsql.sql"
+      ;;
+    *)
+      echo "Database backend not recognized! Exiting."
+      exit 1
+      ;;
+  esac
+
+  echo "Waiting for $DB_BACKEND_NAME ($PDNS_DB_HOST:$DB_PORT) to start..."
+  while ! nc -z $PDNS_DB_HOST $DB_PORT; do
+    sleep 3
+  done
+  echo "$DB_BACKEND_NAME is reachable. Checking database initialization..."
+
+  if eval $DB_CHECK_CMD; then
+    echo "Database already initialized. Skipping schema creation."
+  else
+    echo "Database not initialized. Initializing..."
+    if eval $DB_INIT_CMD; then
+      echo "Database initialized successfully."
+    else
+      echo "Error: Database initialization failed!"
+      exit 1
+    fi
+  fi
 }
 
 # Function to start PowerDNS Server
 function start_pdns {
-  while ! nc -z $PDNS_MYSQL_HOST 3306; do
-    echo "Waiting for MySQL ($MYSQL_HOST:3306) to start..."
-    sleep 3
-  done
-  echo "MySQL is reachable. Checking database initialization..."
-  TABLE_CHECK=$(mysql -h $PDNS_MYSQL_HOST -u $PDNS_MYSQL_USERNAME -p$PDNS_MYSQL_PASSWORD -D $PDNS_MYSQL_DATABASE -e "SHOW TABLES LIKE 'domains';" 2>/dev/null | grep 'domains')
-  # If the table doesn't exist, initialize the database
-  if [[ -z "$TABLE_CHECK" ]]; then
-    echo "Database not initialized. Initializing..."
-    mysql -h $PDNS_MYSQL_HOST -u $PDNS_MYSQL_USERNAME -p$PDNS_MYSQL_PASSWORD -D $PDNS_MYSQL_DATABASE < /usr/share/pdns-backend-mysql/schema/schema.mysql.sql
-    if [ $? -eq 0 ] ; then
-      echo "Database initialized successfully."
-    else
-      echo "Error: Database initialization Failed!"
-      exit 1
-    fi
-  else
-    echo "Database already initialized. Skipping schema creation."
-  fi
+  check_init_powerdns_server
   echo "Generating PowerDNS Authoritative Server configuration..."
   generate_pdns_config
   echo "Starting PowerDNS Authoritative Server..."
