b6b012f4055e7e93fcacfab299afa1dbd27601b6
Saad Ali <engr.saadali786@gmail.com>
2024-07-11 18:38:58 +0500

    Updated PowerDNS module and extravars example for IPv4 and IPv6 records

--- terraform/modules/powerdns/locals.tf
+++ terraform/modules/powerdns/locals.tf
@@ -1,14 +1,14 @@
 locals {
   dns_zones = yamldecode(file(var.dns_config_file))["dns_zones"]
 
-  # Flatten records with reference to their zone
+  # Flatten records with reference to their zone, ensuring unique keys by including the record type
   dns_records = toset(
     flatten(
       [
         for zone in local.dns_zones : [
           for record in zone.records : {
             zone_name = zone.name
-            record_id = "${record.name}.${zone.name}"
+            record_id = "${record.name}.${zone.name}.${record.type}"
             name      = record.name
             type      = record.type
             ttl       = record.ttl
--- terraform/modules/powerdns/main.tf
+++ terraform/modules/powerdns/main.tf
@@ -10,7 +10,7 @@ resource "powerdns_record" "record" {
   for_each = { for rec in local.dns_records : rec.record_id => rec }
 
   zone    = powerdns_zone.primary[each.value.zone_name].name
-  name    = each.key
+  name    = "${each.value.name}.${each.value.zone_name}"
   type    = each.value.type
   ttl     = each.value.ttl
   records = [each.value.content]
